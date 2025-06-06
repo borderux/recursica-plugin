f61b3f94f910021b525b62f706f0775a99357769
jonathan <jonathan@borderux.com>
2025-06-06 12:19:43 -0700

    add storybook and chromatic

---
+++ .storybook/preview.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import type { Preview } from '@storybook/react-vite';
+import { ThemeProvider } from '../src/ui-kit';
+import { Themes } from '../src/recursica/RecursicaRecursicaThemes.css';
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+
+    a11y: {
+      // 'todo' - show a11y violations in the test UI only
+      // 'error' - fail CI on a11y violations
+      // 'off' - skip a11y checks entirely
+      test: 'todo',
+    },
+  },
+  decorators: [
+    (story) => <ThemeProvider themeClassname={Themes.Default.Light}>{story()}</ThemeProvider>,
+  ],
+};
+
+export default preview;
