003103a5eabd78565dd02381308436a6ef2be377
Jonathan Mojica <168861268+jonathan-mojica@users.noreply.github.com>
2025-06-16 16:54:31 -0700

    Files committed by Recursica recursica-bundle.json

--- src/recursica/RecursicaRecursicaThemes.css.ts
+++ src/recursica/RecursicaRecursicaThemes.css.ts
@@ -7,14 +7,8 @@ Do NOT edit these files directly
 
 For more information about Recursica, go to https://recursica.com
 */
-export { DefaultLightTheme } from './RecursicaRecursicaDefaultLightTheme.css';
-import { DefaultLightTheme } from './RecursicaRecursicaDefaultLightTheme.css';
-export { DefaultDarkTheme } from './RecursicaRecursicaDefaultDarkTheme.css';
-import { DefaultDarkTheme } from './RecursicaRecursicaDefaultDarkTheme.css';
 
 export const Themes = {
-  Default: {
-    Light: DefaultLightTheme,
-    Dark: DefaultDarkTheme,
-  },
-};
+  
+  }
+}
